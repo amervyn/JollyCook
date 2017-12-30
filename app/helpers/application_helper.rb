cf7c51d7298c6580e240b8ef53c674ab8db34352
amervyn <amervyn14@gmail.com>
2017-12-30 23:35:24 +0000

    random bg image for landing page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+    def randomized_background_image
+        images = [view_context.image_path("la_table_thanksgiving_table.png")]
+        images[rand(images.size)]
+      end
 end
