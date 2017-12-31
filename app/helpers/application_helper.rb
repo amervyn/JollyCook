087533166e9d5b0333c5ce9afcd66759edecb733
amervyn <amervyn14@gmail.com>
2017-12-31 00:43:09 +0000

    added favicon and random image loads and removed social buttons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def randomized_background_image
-        images = [view_context.image_path("la_table_thanksgiving_table.png")]
+        images = [asset_path("la_table_thanksgiving_table.png"),asset_path("skosh-restaurant-york-small-plate-1170x500.jpg"),asset_path("iStock_000011203427Small11.jpg")]
         images[rand(images.size)]
       end
 end
