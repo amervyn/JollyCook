dfbb10d751881f12ed2a49626014334f7d5cae93
amervyn <amervyn14@gmail.com>
2018-01-14 20:08:00 +0000

    flash message deleted

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -29,8 +29,8 @@ class RegistrationsController < Devise::RegistrationsController
     @user.password=params[:user][:password]
     @user.password_confirmation=params[:user][:password_confirmation]
     if @user.save
-      flash[:notice] = "You have signed up successfully. If enabled, a confirmation was sent to your e-mail."
-      redirect_to root_url
+      #flash[:notice] = "You have signed up successfully. If enabled, a confirmation was sent to your e-mail."
+      redirect_to  root_url
     else
       render :action => :new
     end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,13 @@ module ApplicationHelper
         images = [asset_path("la_table_thanksgiving_table.png"),asset_path("skosh-restaurant-york-small-plate-1170x500.jpg"),asset_path("iStock_000011203427Small11.jpg")]
         images[rand(images.size)]
       end
+
+      def flash_class(level)
+        case level
+            when :notice then "alert alert-info"
+            when :success then "alert alert-success"
+            when :error then "alert alert-error"
+            when :alert then "alert alert-error"
+        end
+    end
 end
